af4d27340295c06a670f27d962c3115788e34edf
MioArchive <emilioramirezziegler@gmx.de>
2025-04-03 22:35:36 +0200

    1.ß

--- src/main/java/net/javamio/playerkits/PlayerKits.java
+++ src/main/java/net/javamio/playerkits/PlayerKits.java
@@ -1,10 +1,21 @@
 package net.javamio.playerkits;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
+@Getter
 public class PlayerKits extends JavaPlugin {
+
+    @Getter
+    public static PlayerKits instance;
+    public static Logger LOGGER = Logger.getLogger("PlayerKits");
 
     @Override
     public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
     }
 }
