c0ee5bd666891eb9ba5b3c41528b232e5e1b6df7
Mio <140413118+MioArchive@users.noreply.github.com>
2025-05-22 21:57:48 +0200

    Add initial project structure and build configuration
    This commit sets up the initial structure for the PlayerKits plugin, including Gradle build files, dependencies, IntelliJ configurations, and a basic plugin implementation. The project targets Java 21 and includes runtime configurations for PaperMC and CommandAPI.

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.run.paper)
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+java {
+    disableAutoTargetJvm()
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+    compileOnly(libs.commandapi)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+}
+
+group = "net.javamio.playerkits"
+version = "1.0"
+description = "A personal user kit's plugin."
+
+tasks {
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveFileName = "${rootProject.name}-${project.version}.jar"
+        archiveClassifier = null
+
+        manifest {
+            attributes["Implementation-Version"] = rootProject.version
+        }
+    }
+
+    assemble {
+        dependsOn(shadowJar)
+    }
+
+    withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 17
+    }
+
+    withType<Javadoc>() {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    defaultTasks("build")
+
+    val version = "1.21.1"
+    val javaVersion = JavaLanguageVersion.of(21)
+
+    val jvmArgsExternal = listOf(
+        "-Dcom.mojang.eula.agree=true"
+    )
+
+    runServer {
+        minecraftVersion(version)
+        runDirectory = rootDir.resolve("run/paper/$version")
+
+        javaLauncher = project.javaToolchains.launcherFor {
+            languageVersion = javaVersion
+        }
+
+        downloadPlugins {
+            url("https://github.com/JorelAli/CommandAPI/releases/download/9.5.3/CommandAPI-9.5.3.jar")
+        }
+
+        jvmArgs = jvmArgsExternal
+    }
+}
---
+++ src/main/java/net/javamio/playerkits/PlayerKits.java
@@ -0,0 +1,10 @@
+package net.javamio.playerkits;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerKits extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
